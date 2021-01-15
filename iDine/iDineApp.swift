5a594f77fe68b66946cfbf786543c1dfa4a9e5bc
Tal Spektor <talspektor@gmail.com>
2021-01-15 12:45:14 +0200

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Tal talspektor on 15/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
